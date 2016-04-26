8db1b0bf73e45fd9ba9663d8ade54344afa902f7
Rachell Calhoun <rachell.calhoun@gmail.com>
2016-04-26 16:11:06 +0900

    comment on same pages as question

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,18 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/post_detail.html', {'post': post, 'form': form})
 
 def notice_list(request):
     notices = Notice.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
@@ -109,20 +120,20 @@ def notice_remove(request, pk):
     notice.delete()
     return redirect('blog.views.notice_list')
 
-def add_comment_to_post(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.author = request.user
-            comment.post = post
-            comment.save()
+# def add_comment_to_post(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     if request.method == "POST":
+#         form = CommentForm(request.POST)
+#         if form.is_valid():
+#             comment = form.save(commit=False)
+#             comment.author = request.user
+#             comment.post = post
+#             comment.save()
 
-            return redirect('blog.views.post_detail', pk=post.pk)
-    else:
-        form = CommentForm()
-    return render(request, 'blog/add_comment_to_post.html', {'form': form, 'post': post})
+#             return redirect('blog.views.post_detail', pk=post.pk)
+#     else:
+#         form = CommentForm()
+#     return render(request, 'blog/post_detail.html', {'form': form, 'post': post})
 
 @login_required
 def comment_approve(request, pk):
